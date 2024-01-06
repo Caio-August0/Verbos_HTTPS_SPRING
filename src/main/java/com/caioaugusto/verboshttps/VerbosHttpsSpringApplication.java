a9bd62b98e2caed9c14c4a16dcd2658979705a35
Caio Augusto <caioaugustobr0106@gmail.com>
2024-01-05 23:30:19 -0300

    initial project version

---
+++ src/main/java/com/caioaugusto/verboshttps/VerbosHttpsSpringApplication.java
@@ -0,0 +1,13 @@
+package com.caioaugusto.verboshttps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VerbosHttpsSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VerbosHttpsSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/caioaugusto/verboshttps/VerbosHttpsSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caioaugusto.verboshttps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VerbosHttpsSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
